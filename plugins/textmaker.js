00295ca7f4f2f61e524a46507f6788c2e6b3df77
chamindu pushpika <72345334+PYMaster-ft@users.noreply.github.com>
2021-07-25 14:46:16 +0530

    Update textmaker.js

--- plugins/textmaker.js
+++ plugins/textmaker.js
@@ -164,7 +164,7 @@ Asena.addCommand({pattern: 'txttoimg$', fromMe: wk, desc: desc_msg}, (async (mes
     await message.client.sendMessage(message.jid,msg, MessageType.text, { quoted: message.data })
 }));
 Asena.addCommand({pattern: 'devil ?(.*)', fromMe: wk, dontAddCommandList: true}, (async (message, match) => {
-    thiccysapi.textpro("https://textpro.me/create-neon-devil-wings-text-effect-online-free-1014.html",
+    thiccysapi.textpro("https://textpro.me/captain-america-text-effect-905.html",
         `${match[1]}`
         ).then(async (data) => { 
           try { 
